e826b80af652c2863f9d8273f7800e1de82b5f5c
Leo Singer <leo.singer@ligo.org>
2014-05-31 21:02:56 -0700

    Add script to calculate Fisher matrix for autocorrelation likelihood

---
+++ scripts/autocorrelation_fisher_matrix.py
@@ -0,0 +1,137 @@
+"""Compute Fisher matrix for autocorrelation likelihood, using a TaylorF2
+signal model."""
+from __future__ import division
+import numpy as np
+import scipy.integrate
+import scipy.interpolate
+import scipy.special
+import multiprocessing
+import itertools
+from lalinference.bayestar import filter
+from lalinference.bayestar import timing
+
+def abs2(x):
+    return np.square(np.real(x)) + np.square(np.imag(x))
+
+mass1 = 1.4
+mass2 = 1.4
+f_low = 40
+S = timing.get_noise_psd_func('H1')
+
+# Compute Fisher matrix elements for full signal (for comparison).
+signal_model = timing.SignalModel(mass1, mass2, S, f_low,
+    *timing.get_approximant_and_orders_from_string('TaylorF2threePointFivePN'))
+w1 = signal_model.get_sn_moment(1)
+w2 = signal_model.get_sn_moment(2)
+crlb1 = 1 / np.sqrt(w2 - np.square(w1))
+
+# Compute 1 second of autocorrelation sequence.
+# FIXME: make this the longest lag that we are going to integrate over.
+acor_series, sample_rate = filter.autocorrelation(
+    mass1, mass2, S, f_low, 1,
+    *timing.get_approximant_and_orders_from_string('TaylorF2threePointFivePN'))
+
+# Augment autocorrelation sequence with negative time lags.
+n = len(acor_series)
+t = np.arange(1-n, n) / sample_rate
+acor_series = np.concatenate((np.conj(acor_series[:0:-1]), acor_series))
+
+# Construct spline interpolant and derivatives for real part.
+re_acor = scipy.interpolate.UnivariateSpline(t, acor_series.real, s=0, k=5)
+re_acor_deriv = re_acor.derivative(1)
+re_acor_deriv2 = re_acor.derivative(2)
+
+# Construct spline interpolant and derivatives for imaginary part.
+im_acor = scipy.interpolate.UnivariateSpline(t, acor_series.imag, s=0, k=5)
+im_acor_deriv = im_acor.derivative(1)
+im_acor_deriv2 = im_acor.derivative(2)
+
+# Put real and imaginary parts back together.
+def acor(t): return re_acor(t) + 1j * im_acor(t)
+def acor_deriv(t): return re_acor_deriv(t) + 1j * im_acor_deriv(t)
+def acor_deriv2(t): return re_acor_deriv2(t) + 1j * im_acor_deriv2(t)
+
+def weight(a2, rho):
+    rho2 = np.square(rho)
+    Iarg = 0.25 * rho2 * a2
+    exp = np.exp(0.5 * rho2 * (a2 - 1))
+    return 0.5 * exp * (scipy.special.i0e(Iarg) + scipy.special.i1e(Iarg))
+
+def den(t, rho):
+    a = acor(t)
+    a2 = abs2(a)
+    rho2 = np.square(rho)
+    exp = np.exp(0.5 * rho2 * (a2 - 1))
+    return exp * scipy.special.i0e(0.25 * rho2 * a2)
+
+def num_gamma_gamma(t, rho):
+    a = acor(t)
+    a2 = abs2(a)
+    return a2 * weight(a2, rho)
+
+def num_tau_tau(t, rho):
+    a = acor(t)
+    a2 = abs2(a)
+    a_deriv2 = acor_deriv2(t)
+    return -np.real(np.conj(a) * a_deriv2) * weight(a2, rho)
+
+def num_gamma_tau(t, rho):
+    a = acor(t)
+    a2 = abs2(a)
+    a_deriv = acor_deriv(t)
+    return -np.imag(np.conj(a) * a_deriv) * weight(a2, rho)
+
+def fisher_integral((T, num, rho)):
+    args = (rho,)
+    # All of the integrands are even, so we can integrate from 0 to T
+    # instead of from -T to T.
+    n, _ = scipy.integrate.quad(num, 0, T, args, limit=500)
+    d, _ = scipy.integrate.quad(den, 0, T, args, limit=500)
+    return n / d
+
+def map_nested(map, func, *iterables):
+    results = map(func, itertools.product(*iterables))
+    return np.reshape(results, [len(iterable) for iterable in iterables])
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+
+    # t = np.linspace(-1, 1, 32768)
+    # plt.plot(t, abs2(acor(t)), label=r'$\gamma\gamma$')
+    # plt.plot(t, np.imag(np.conj(acor(t)) * acor_deriv(t))/w1, label=r'$\gamma\tau$')
+    # plt.plot(t, -np.real(np.conj(acor(t)) * acor_deriv2(t))/w2, label=r'$\tau\tau$')
+    # plt.legend()
+    # plt.xlim(-0.01, 0.01)
+    # plt.savefig('integrand.pdf')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, aspect=1)
+    snrs = np.logspace(-1, np.log10(30), 100)
+    durations = [0.001, 0.01, 0.1, 1.]
+    integrands = [num_gamma_gamma, num_tau_tau, num_gamma_tau]
+    pool = multiprocessing.Pool()
+    try:
+        integrals = map_nested(
+            pool.map, fisher_integral, durations, integrands, snrs)
+    finally:
+        pool.terminate()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, aspect=1)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('SNR')
+    ax.set_ylabel('RMS timing error (ms)')
+    ax.set_ylim(1e-2, 10)
+    ax.grid()
+
+    for T, data in zip(durations, integrals):
+        F11, F22, F12 = data
+        std_tau = np.sqrt(F11 / (F11 * F22 - np.square(F12))) / snrs
+        ax.plot(snrs, 1e3 * std_tau, label=r'$T={0:g}$'.format(T))
+
+    snrs = np.asarray([1e-1, 1e2])
+    ax.plot(snrs, 1e3 * crlb1 / snrs, '--k', label=r'CRLB')
+
+    plt.legend(loc='upper right')
+    fig.savefig('autocorrelation_std_tau.pdf')
